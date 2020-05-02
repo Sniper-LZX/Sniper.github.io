e9676d8dc6bb83152affdad77b2c0990f7b25644
Sniper-LZX <1017612645@qq.com>
2020-05-02 19:20:44 +0800

    Site updated: 2020-05-02 19:20:42

--- js/src/motion.js
+++ js/src/motion.js
@@ -3,6 +3,17 @@
 $(document).ready(function () {
   NexT.motion = {};
 
+  sidebarToggle: function (integrator) {
+    sidebarToggleMotion.init();
+    integrator.next();
+    // 可以不加if直接显示
+    // 加if的话，要在主题配置文件里面修改 sidebar: always
+    if (CONFIG.sidebar === 'always') {
+       // 模拟点击一下侧边栏按钮
+       sidebarToggleMotion.clickHandler();
+    }
+  };
+
   var sidebarToggleLines = {
     lines: [],
     push: function (line) {
